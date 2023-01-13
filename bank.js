6cbb147a370a5756bc583033ea6efe6ce132f669
malmz90 <70096644+malmz90@users.noreply.github.com>
2023-01-13 15:56:46 +0100

    work.js almost done

--- bank.js
+++ bank.js
@@ -4,15 +4,25 @@ let initiaBalance = 200;
 let loan = 0;
 document.getElementById("balance").innerHTML = initiaBalance;
 
-const balance = Number(document.getElementById("balance").innerHTML);
+const containsOnlyNumbers = (input) => {
+  return /^\d+$/.test(input);
+};
 
 const handleLoan = () => {
-  const userLoanInput = Number(prompt("Type in how much you wish to loan: "));
+  const balance = Number(document.getElementById("balance").innerHTML);
   if (loan > 0) {
-    return alert("you already have a loan");
+    return alert("You already have a loan");
   }
 
-  if (userLoanInput <= balance * 2) {
+  const userLoanInput = prompt("Type in how much you wish to loan: ");
+  let validateInput = containsOnlyNumbers(userLoanInput);
+
+  if (!validateInput) {
+    return alert("Please only use numbers");
+  }
+  let parseInputToNumber = Number(userLoanInput);
+
+  if (parseInputToNumber <= balance * 2) {
     document.getElementById("loan").innerHTML = userLoanInput;
     document.getElementById("loanContainer").style.visibility = "visible";
     loan = userLoanInput;
